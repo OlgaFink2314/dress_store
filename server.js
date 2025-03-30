8b253f048298311de1b502ac0d99123adb4d3a71
Даниил Архипенков <mrdanilow@mail.ru>
2025-03-30 23:44:58 +0300

    up

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false // Для внешних БД обычно требуется SSL
+    }
 });
 
 // Подключаем middleware для работы с cookies
